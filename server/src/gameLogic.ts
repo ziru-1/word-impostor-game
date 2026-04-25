ab81e7301b9ceb1dfaff6cd33b0a790b4b634ef0
Zed <zedcaoile@gmail.com>
2026-04-25 20:12:29 +0800

    feat: add logic to submitRoundDecision

--- server/src/gameLogic.ts
+++ server/src/gameLogic.ts
@@ -18,17 +18,60 @@ export function startGame(room: GameRoom): GameRoom {
     stage: 'playing',
     roundNumber: 1,
     votes: [],
-    roundDecision: [],
+    roundDecisions: [],
   }
 
   return updatedRoom
 }
 
-// export function submitRoundDecision(
-//   room: GameRoom,
-//   playerId: string,
-//   choice: 'skip' | 'vote',
-// ): GameRoom {}
+export function submitRoundDecision(
+  room: GameRoom,
+  playerId: string,
+  choice: 'skip' | 'vote',
+): GameRoom {
+  if (room.roundDecisions.some((player) => player.playerId === playerId)) {
+    throw new Error('Player has already voted')
+  }
+
+  const updatedRoundDecision = room.roundDecisions.concat({ playerId, choice })
+
+  const decisionPlayerIds = new Set(updatedRoundDecision.map((d) => d.playerId))
+  const allPlayersDecided = room.players.every((player) =>
+    decisionPlayerIds.has(player.id),
+  )
+
+  if (!allPlayersDecided) {
+    return {
+      ...room,
+      roundDecisions: updatedRoundDecision,
+    }
+  }
+
+  let voteCount = 0
+  let skipCount = 0
+
+  for (const d of updatedRoundDecision) {
+    if (d.choice === 'vote') voteCount++
+    else if (d.choice === 'skip') skipCount++
+  }
+
+  const majorityVote = voteCount > skipCount
+  const isFinalRound = room.roundNumber === 3
+
+  if (majorityVote || isFinalRound) {
+    return {
+      ...room,
+      roundDecisions: updatedRoundDecision,
+      stage: 'voting',
+    }
+  }
+
+  return {
+    ...room,
+    roundNumber: room.roundNumber + 1,
+    roundDecisions: [],
+  }
+}
 
 // export function castVote(
 //   room: GameRoom,
